735bb41c9896413a3484b0ca0a73cbab96fefb9f
Dylan Thiermann <dylant96@gmail.com>
2021-02-25 08:34:38 -0800

    basic print functions tested

--- go_second.py
+++ go_second.py
@@ -2,39 +2,29 @@ import re
 
 length = 5
 
-example_board = [[0 for x in range(length)] for y in range(length)]
+example_board = [[ ['empty', '0'] for x in range(length)] for y in range(length)]
 
-example_board[2][1] = 3
 
 def print_board(board):
     for y in range(len(board)):
         row = ''
         for x in range(len(board)):
-            row = row + str(board[x][y])+' '
+            row = row + board[x][y][0] + ' '
 
         print(row)
+        
+def print_everything(board):
+    for y in range(len(board)):
+        row = ''
+        for x in range(len(board)):
+            row = row + board[x][y][0] + ' '+ board[x][y][1] + ' '
 
-def adjacent(x,y,board):
-    neighbors = [[x,y+1],[x,y-1],[x+1,y],[x-1,y]]
-
-    real_neighbors = []
-
-    for point in neighbors:
-        if point[0] < 0 or point[0] >= len(board):
-            pass
-        elif point[1] < 0 or point[1] >= len(board):
-            pass
-        else:
-            real_neighbors.append(point)
-
-    return real_neighbors
-                
-            
+        print(row)
     
-
-    for point in neighbors:
-        for coord in point:
-            if coord < 0 or coord >= len(board):
-                neighbors.remove(point)
-
-    return neighbors
+def make_move(color, board, x, y, n):
+    if board[x][y][0] != 'empty':
+        print('error: point occupied')
+    else:
+        board[x][y] = [color,str(n)]
+        
+        
