e343fd4c7f36fc97edafb55913702bfe0c6a4dc8
Dylan Thiermann <dylant96@gmail.com>
2021-02-25 09:35:07 -0800

    adjacent function works

--- go_second.py
+++ go_second.py
@@ -17,7 +17,7 @@ def print_everything(board):
     for y in range(len(board)):
         row = ''
         for x in range(len(board)):
-            row = row + board[x][y][0] + ' '+ board[x][y][1] + ' '
+            row = row + board[x][y][0] + ' '+ str(board[x][y][1]) + ' '
 
         print(row)
     
@@ -25,6 +25,26 @@ def make_move(color, board, x, y, n):
     if board[x][y][0] != 'empty':
         print('error: point occupied')
     else:
-        board[x][y] = [color,str(n)]
+        board[x][y] = [color, n]
+
+
+def adjacent(x,y,board):
+    
+    nearby = [[x,y+1],[x,y-1],[x+1,y],[x-1,y]]
+
+    neighbors = []
+    
+    for point in nearby:
+        x_valid = point[0] in range(0, len(board))
+        y_valid = point[1] in range(0, len(board))
+        if x_valid and y_valid:
+            neighbors.append(point)
+
+    return neighbors
+
+
         
-        
+
+    
+
+    
